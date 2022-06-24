6b5fd79b77bb40d9b4847d2b489f92806038b7b7
nataliaclark01 <79187383+nataliaclark01@users.noreply.github.com>
2022-06-24 19:01:37 -0400

    fixing friend_email

--- phase1.sql
+++ phase1.sql
@@ -1,11 +1,8 @@
-DROP DATABASE photoshare;
 CREATE DATABASE IF NOT EXISTS photoshare;
 USE photoshare;
-DROP TABLE IF EXISTS Pictures CASCADE;
-DROP TABLE IF EXISTS Users CASCADE;
 
 CREATE TABLE Users (
-    user_id int4  AUTO_INCREMENT,
+    user_id int4 AUTO_INCREMENT,
     email varchar(255) UNIQUE,
     password varchar(255),
     fname varchar(255), 
@@ -15,6 +12,7 @@ CREATE TABLE Users (
     gender varchar(255),
     PRIMARY KEY (user_id)
 );
+
 CREATE TABLE Albums ( 
 	album_id int4 auto_increment, 
     album_name varchar(255),
@@ -22,11 +20,13 @@ CREATE TABLE Albums (
     date_ofc date, 
     PRIMARY KEY (album_id)
 );
+
 CREATE TABLE Friends ( 
-	friend_id int4, 
-    user_id int4 REFERENCES Users(user_id) ON DELETE CASCADE,
-    PRIMARY KEY (friend_id)
+    friend_email varchar(255),
+    email int4 REFERENCES Users(email) ON DELETE CASCADE,
+    PRIMARY KEY (friend_email, email)
 );
+
 CREATE TABLE Pictures (
   picture_id int4  AUTO_INCREMENT,
   album_id int4,
@@ -35,47 +35,55 @@ CREATE TABLE Pictures (
   INDEX upid_idx (album_id),
   PRIMARY KEY (picture_id)
 );
+
 CREATE TABLE Comments (
 	comm_id int4 auto_increment,
-    friend_id int4 REFERENCES Friends(friend_id) ON DELETE CASCADE,
+    friend_email int4 REFERENCES Friends(friend_email) ON DELETE CASCADE,
     comm_text varchar(255), 
     comm_date date,
     PRIMARY KEY (comm_id)
 );
+
 CREATE TABLE Tags ( 
 	tag_id int4,
 	word varchar(255), 
     PRIMARY KEY (tag_id)
 );
+
 CREATE TABLE Makes (
 	user_id int4 REFERENCES Users(user_id) ON DELETE CASCADE, 
     album_id int4 REFERENCES Albums(album_id) ON DELETE CASCADE,
     PRIMARY KEY (user_id, album_id)
 );
+
 CREATE TABLE Contain (
 	album_id int4 REFERENCES Albums(album_id) ON DELETE CASCADE, 
     picture_id int4 REFERENCES Pictures(picture_id) ON DELETE CASCADE,
     PRIMARY KEY (picture_id, album_id)
 );
+
 CREATE TABLE Comm_On (
 	comm_id int4 REFERENCES Comments(comm_id) ON DELETE CASCADE, 
     picture_id int4 REFERENCES Pictures(picture_id) ON DELETE CASCADE,
     PRIMARY KEY (comm_id, picture_id)
 );
+
 CREATE TABLE Comm (
 	comm_id int4 REFERENCES Comments(comm_id) ON DELETE CASCADE, 
-    friend_id int4 REFERENCES Friends(friend_id) ON DELETE CASCADE,
-    PRIMARY KEY (comm_id, friend_id)
+    friend_email int4 REFERENCES Friends(friend_email) ON DELETE CASCADE,
+    PRIMARY KEY (comm_id, friend_email)
 );
+
 CREATE TABLE Associated (
 	picture_id int4 REFERENCES Pictures(picture_id) ON DELETE CASCADE, 
     tag_id int4 REFERENCES Tags(tag_id) ON DELETE CASCADE,
     PRIMARY KEY (picture_id, tag_id)
 );
+
 CREATE TABLE Liked (
-	friend_id int4 REFERENCES Friends(friend_id) ON DELETE CASCADE, 
+	friend_email int4 REFERENCES Friends(friend_email) ON DELETE CASCADE, 
     picture_id int4 REFERENCES Pictures(picture_id) ON DELETE CASCADE,
-    PRIMARY KEY (friend_id, picture_id)
+    PRIMARY KEY (friend_email, picture_id)
 );
 
 
